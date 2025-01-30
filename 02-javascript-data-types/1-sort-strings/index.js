1fd6d9b4b4cba779a6ef6b71ee41782278db0af7
anton.sadovsky <anton.sadovsky.97@gmail.com>
2025-01-30 15:14:54 +0300

    task sortStrings - solved

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,22 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+    const copiedArray = arr.slice();
 
+    if (param === 'asc') {
+        return copiedArray.sort((a, b) => comperer(a, b));
+    }
+    else {
+        return copiedArray.sort((a, b) => comperer(b, a));
+    }
+}
+
+function comperer(a, b) {
+    let result = a.localeCompare(b, undefined, { sensitivity: 'base' });
+
+    if (result == 0) {
+        result = -1 * a.localeCompare(b, undefined, { sensitivity: 'case' });
+    }
+
+    return result;
 }
